b01919b2e0a87c981c695f221918e7174c873e5e
Igor Silva <igorsilvadeveloper@gmail.com>
2023-02-02 23:39:09 -0300

    Initial Commit

---
+++ MatchWords/MatchWordsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatchWordsApp.swift
+//  MatchWords
+//
+//  Created by Igor Silva on 02/02/23.
+//
+
+import SwiftUI
+
+@main
+struct MatchWordsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
