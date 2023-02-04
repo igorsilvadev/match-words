d3823995d8addd20c06b57802c982604cb26df66
Igor Silva <igorsilvadeveloper@gmail.com>
2023-02-04 16:40:19 -0300

    feat: Created mechaninc and button change colors. There is bugs to fix

--- MatchWords/ContentView.swift
+++ MatchWords/ContentView.swift
@@ -6,40 +6,82 @@
 //
 
 import SwiftUI
+import Combine
+class ViewModel: ObservableObject {
+    @Published var key = ""
+    @Published var value = ""
+    @Published var keys: [String] = []
+    @Published var values: [String] = []
+    @Published var dictionary: [String: String] = ["Oi": "hi", "pão": "bread", "carro": "car", "casa": "house"]
+    var cancellables = Set<AnyCancellable>()
+    
+    init() {
+        
+        keys = dictionary.keys.map{$0}.shuffled()
+        values = dictionary.values.map{$0}.shuffled()
+        
+        $key.zip($value).sink {  key, value in
+            print("Combinação key:\(key) value:\(value)")
+            if (!value.isEmpty && !key.isEmpty) && self.dictionary[key] == value {
+                self.key = ""
+                self.value = ""
+                self.keys.removeAll { $0 == key}
+                self.values.removeAll { $0 == value}
+            
+            }
+        }.store(in: &cancellables)
+    }
+}
 
 struct ButtonView: View {
     var word: String
+    @Binding var matchWord: String
+    @State private var backgroundColor: Color = .gray
+    @State var isPressed = false
     var body: some View {
-        Button(word) {
-            
+        Button {
+            if !isPressed {
+                isPressed = true
+                matchWord = word
+            } else {
+                isPressed = false
+                matchWord = ""
+            }
+        } label: {
+            ZStack {
+                Rectangle()
+                    .cornerRadius(10)
+                    .foregroundColor(isPressed ? .brown : backgroundColor)
+                    .frame(width: 120, height: 75)
+                Text(word)
+                    .foregroundColor(.black)
+            }
         }
-        .buttonStyle(.plain)
-        .background(content: {
-            Rectangle()
-                .foregroundColor(.gray)
-                .frame(width: 75, height: 50)
-        })
-        .padding()
     }
 }
 
 
 struct ContentView: View {
-    var dictionary: [String: String] = ["Oi": "hi", "pão": "bread", "carro": "car", "casa": "house"]
+    @StateObject private var viewModel = ViewModel()
+    
     var body: some View {
         
         VStack {
             HStack {
                 VStack {
-                    ForEach(dictionary.keys.map{$0}, id: \.self) { word in
-                        ButtonView(word: word)
+                    ForEach(viewModel.keys, id: \.self) { word in
+                        ButtonView(word: word, matchWord: $viewModel.key)
+                            .padding(10)
                     }
                 }
+                .padding(.trailing)
                 VStack {
-                    ForEach(dictionary.values.map{$0}, id: \.self) { word in
-                        ButtonView(word: word)
+                    ForEach(viewModel.values, id: \.self) { word in
+                        ButtonView(word: word, matchWord: $viewModel.value)
+                            .padding(10)
                     }
                 }
+                .padding(.leading)
             }
         }
     }
@@ -48,6 +90,6 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
-            .preferredColorScheme(.dark)
+            .preferredColorScheme(.light)
     }
 }
